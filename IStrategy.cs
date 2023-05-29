59095aa81beeb52fc089162d2e1edf3655878ed7
shtrih2k4 <andriizhevahin@gmail.com>
2023-05-29 21:51:33 +0300

    Додав паттерн Стратегію та дві його реалізації(через LINQ to XML та SAX) для зчитування xml файлу

---
+++ IStrategy.cs
@@ -0,0 +1,15 @@
+﻿using Lab2;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2 { 
+
+    internal interface IStrategy
+    {
+        List<PaperWork> Algorithm(PaperWork student, string path);
+    }
+}
---
+++ Linq.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab2
+{
+    internal class Linq:IStrategy
+    {
+        List<PaperWork> information = new List<PaperWork>();
+        XDocument doc = new XDocument();
+        public Linq(string path)
+        {
+            doc=XDocument.Load(path);
+        }
+        public List<PaperWork> Algorithm(PaperWork student,string path)
+        {
+            List<XElement> match=(from val in doc.Descendants("student")
+                                  where
+                                  ((student.Course==null||student.Course==val.Parent.Parent.Attribute("COURSE").Value)&&
+                                  (student.Group==null||student.Group==val.Parent.Attribute("GROUP").Value) &&
+                                     (student.Auditory == null || student.Auditory == val.Attribute("AUDITORY").Value) &&
+                                     (student.Surname == null || student.Surname == val.Attribute("SURNAME").Value) &&
+                                     (student.Name == null ||student.Name == val.Attribute("NAME").Value) &&
+                                     (student.PhoneNumber == null || student.PhoneNumber == val.Attribute("PHONENUMBER").Value))
+                                  select val).ToList();
+            foreach(XElement obj in match)
+            {
+                PaperWork paperwork=new PaperWork();
+                paperwork.Course = obj.Parent.Parent.Attribute("COURSE").Value;
+                paperwork.Group = obj.Parent.Attribute("GROUP").Value;
+                paperwork.Auditory = obj.Attribute("AUDITORY").Value;
+                paperwork.Surname = obj.Attribute("SURNAME").Value;
+                paperwork.Name = obj.Attribute("NAME").Value;
+                paperwork.PhoneNumber = obj.Attribute("AMOUNT").Value;
+                information.Add(paperwork);
+            }
+            return information;
+        }
+    }
+}
